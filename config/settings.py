8543b8e16e846cfa7999825ccb2eaee9d50138ba
manish ranjan <manishranjan.thehunk@gmail.com>
2020-06-14 22:12:47 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gph%0*$pcq2#p9lts5y@$kg&i(+nrmhey4zy&8wwq7a*^rk=15'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['manishranjan.pythonanywhere.com']
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'june13_auth.apps.June13AuthConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -120,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/manishranjan/june13/static'
 
 # Tell Django about the custom `User` model we created. The string
 # `june13_auth.User` tells Django we are referring to the `User` model in
@@ -134,3 +137,5 @@ REST_FRAMEWORK = {
            'june13_auth.backends.JWTAuthentication',
         ),
 }
+
+CORS_ORIGIN_ALLOW_ALL  = True
