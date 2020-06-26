fc446e378c66d5de67aa44148e04d88af52c926c
manish ranjan <manishranjan.thehunk@gmail.com>
2020-06-26 10:03:09 +0530

    Password reset changes

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/account/', include('june13_auth.urls')),
-    path('api/account/', include('django.contrib.auth.urls')),
+    path('api/account/password_reset', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html', subject_template_name="accounts/password_reset_subject.txt"),name="password_reset"),
+    path('api/account/password_reset/done',auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),name="password_reset_done"),
+    path('api/account/password_reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),name="password_reset_complete"),
+    path('api/account/password_reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),name="password_reset_confirm"),
 ]
